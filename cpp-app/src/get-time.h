b3a292c9dfe4ffaa04655631ce77ebdf8b352c99
Nikolai Merritt <22347025+nikolaimerritt@users.noreply.github.com>
2022-10-05 11:37:58 +0100

    Add files via upload

---
+++ cpp-app/src/get-time.h
@@ -0,0 +1,7 @@
+#ifndef GET_TIME_H
+#define GET_TIME_H
+
+#include <string>
+std::string get_time();
+
+#endif
---
+++ cpp-app/src/main.cpp
@@ -0,0 +1,10 @@
+#include "get-time.h"
+#include <iostream>
+
+int main() {
+    std::string time_string = get_time();
+    std::cout << "Hello world!" << std::endl;
+    std::cout << "The time is " << time_string << std::endl;
+
+    return 0;
+}
