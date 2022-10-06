d4edef66a38597f528806ac3be95df216534fc1f
Nikolai Merritt <22347025+nikolaimerritt@users.noreply.github.com>
2022-10-06 09:08:15 +0000

    tidied

--- cpp-app/get-time.cpp
+++
@@ -1,7 +0,0 @@
-#include "get-time.h"
-#include <ctime>
-
-std::string get_time() {
-  std::time_t result = std::time(nullptr);
-  return std::string(std::asctime(std::localtime(&result)));
-}
--- cpp-app/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-//    std::string time_string = get_time();
-    std::cout << "Hello world!" << std::endl;
-//    std::cout << "The time is " << time_string << std::endl;
-
-    return 0;
-}
